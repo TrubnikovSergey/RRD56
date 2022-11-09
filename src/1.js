b4025779b1ecfd4b4f7e3df0ccff55bd07fd6dbc
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-11-09 17:15:29 +0300

    router5

---
+++ src/1.js
@@ -0,0 +1,12 @@
+// Home Page
+// 	1. NavLink to user list
+// Users List page
+// 	1. NavLink to user page
+// 	2. NavLink to home page
+// User page
+// 	1. NavLink to user edit page
+// 	2. NavLink to users list
+// User edit page
+// 	1. NavLink to user page
+// 	2. NavLink to another user page
+// 	3. NavLink to Users list page
--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Link, Redirect, Route, Switch, useParams } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>HomePage</h1>;<Link to="/users">Users list</Link>
+    </>
+  );
+};
+
+const UsersList = () => {
+  return (
+    <>
+      <Link to="/">Home page</Link>
+      <h1>UsersList</h1>
+      <ul>
+        <li>
+          <Link to="/users/1">User 1</Link>
+        </li>
+        <li>
+          <Link to="/users/2">User 2</Link>
+        </li>
+        <li>
+          <Link to="/users/3">User 3</Link>
+        </li>
+        <li>
+          <Link to="/users/4">User 4</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const UserPage = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h1>User edit page</h1>
+      <br />
+      <Link to={`/users/${userId}/edit`}>User edit page</Link>
+      <h1>Users list</h1>;<Link to="/users">Users list</Link>
+    </>
+  );
+};
+
+const UserEditPage = () => {
+  const { userId } = useParams();
+  return (
+    <>
+      <h1>User edit page</h1>
+      <ul>
+        <li>
+          <Link to={`/users/${userId}`}>User page</Link>
+        </li>
+        <li>
+          <Link to={`/users/${+userId + 1}`}>Another user page</Link>
+        </li>
+        <li>
+          <Link to="/users">Users list</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/users" exact component={UsersList} />
+        <Route path="/users/:userId" exact component={UserPage} />
+        <Route path="/users/:userId/edit" exact component={UserEditPage} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 }
 
